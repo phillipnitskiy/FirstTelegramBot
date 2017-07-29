6924797b08c7d8a857a86c3285bfde88d6d9943c
Philip <phillipnitskiy@gmail.com>
2017-07-29 14:28:55 +0300

    Add project files.

---
+++ FirstTelegramBot/Models/AppSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace FirstTelegramBot.Models
+{
+    public static class AppSettings
+    {
+        public static string Url { get; } = "https://kelevrabotapp.azurewebsites.net:443/{0}";
+
+        public static string Name { get; } = "kelevra_bot";
+
+        public static string Key { get; } = "433357514:AAGlFdksVYHkfVeLdnr3NS5vBqEANIM3VUw";
+
+    }
+}
